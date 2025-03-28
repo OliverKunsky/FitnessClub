84b251ed45bd36fa9a5144ec26b351a69bdef02d
OliverKunsky <oliver.kunsky@student.ukf.sk>
2025-03-28 15:07:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<!--
-Fitness Template
-http://www.templatemo.com/tm-487-fitness
--->
-<title>Fitness - Responsive HTML Template</title>
-<meta name="description" content="">
-<meta name="author" content="">
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/animate.css">
-<link rel="stylesheet" href="css/font-awesome.min.css">
-<link rel="stylesheet" href="css/owl.theme.css">
-<link rel="stylesheet" href="css/owl.carousel.css">
-
-<!-- Main css -->
-<link rel="stylesheet" href="css/style.css">
-
-<!-- Google Font -->
-<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Lora:700italic' rel='stylesheet' type='text/css'>
-
-</head>
 <body data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
 
 
